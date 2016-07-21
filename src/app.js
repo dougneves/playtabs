e0a4655081ef84b8c2eca1cffaa82561a95a9e29
Douglas D. F. das Neves <douglas@evaltec.com.br>
2016-07-21 14:59:03 -0300

    Preparing app for modulus

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+var appport = process.env.PORT;
+if(!appport) appport = 8284;
+
 var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(8284);
-console.log('Server running...');
+}).listen(appport);
+console.log('Server running in port '+appport);
