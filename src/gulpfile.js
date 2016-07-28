b89185cd0ebba9887c43709255038820ed477bef
Douglas Neves <dougneves@gmail.com>
2016-07-28 01:56:47 -0300

    adicionando os modulos less

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,4 +1,14 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./pub/css'));
+});
 
 gulp.task('default', function() {
   console.log("rodando o default.");
